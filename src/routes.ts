443c681758a439f550d78f0771b3b8119e50adf6
Riccardo Barbatti <barbatti.riccardo@gmail.com>
2022-06-23 15:53:58 +0200

    Update Routes
        .post('/api/logout', Logout)
        .get('/api/user', authMiddleware, Me)
        .post('/api/forgot', Forgot)
        .post('/api/reset', Reset);

--- src/routes.ts
+++ src/routes.ts
@@ -1,26 +1,18 @@
-import { Router } from "https://deno.land/x/oak/mod.ts";
-import {Hello, Register} from "./controllers/authController.ts";
 
+import {Router} from 'https://deno.land/x/oak@v6.3.1/mod.ts';
+import {Login, Register, Me, Logout} from './controllers/authController.ts';
+import {authMiddleware} from "./middlewares/authMiddleware.ts";
+import {Forgot, Reset} from "./controllers/forgotController.ts";
 
 const router = new Router();
 
-//hello test
-router.get('/api/hello', Hello)
-//register
-router.post('/api/register', Register);
-
-
-
-
-
+router
+    .post('/api/register', Register)
+    .post('/api/login', Login)
+    .post('/api/logout', Logout)
+    .get('/api/user', authMiddleware, Me)
+    .post('/api/forgot', Forgot)
+    .post('/api/reset', Reset);
 
 
 export default router;
-
-
-//check route
-// router.get('/', ({response} ) => {
-//     response.body = 'HomePage'
-// }).get('/new', ({response}) => {
-//     response.body = 'New Page'
-// });
