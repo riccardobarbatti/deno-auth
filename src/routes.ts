2375ec853dce56856f01b3fb63181c32c5cddb05
Riccardo Barbatti <barbatti.riccardo@gmail.com>
2022-06-23 15:57:22 +0200

    Update routes.ts
    Test Jwt

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,7 @@
 import {Router} from 'https://deno.land/x/oak@v6.3.1/mod.ts';
 import {Login, Register, Me, Logout} from './controllers/authController.ts';
 import {authMiddleware} from "./middlewares/authMiddleware.ts";
-import {Forgot, Reset} from "./controllers/forgotController.ts";
+
 
 const router = new Router();
 
@@ -11,8 +11,6 @@ router
     .post('/api/login', Login)
     .post('/api/logout', Logout)
     .get('/api/user', authMiddleware, Me)
-    .post('/api/forgot', Forgot)
-    .post('/api/reset', Reset);
 
 
 export default router;
